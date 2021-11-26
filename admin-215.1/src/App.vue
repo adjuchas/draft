2760b4bf1580b8d638be2485734b13b185807a56
Willianxian <1064863952@qq.com>
2021-11-26 18:49:12 +0800

    Update App.vue

--- admin-215.1/src/App.vue
+++ admin-215.1/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-  <home :allmessage="allrecodes"></home>
+  <home></home>
   </div>
 </template>
 
@@ -9,7 +9,6 @@
 
 import home from "@/components/home";
 
-//import {sendRedirect} from "@/umtils/oauthTool";
 
 export default {
   name: 'App',
@@ -18,38 +17,18 @@ export default {
     home
   },
  beforeCreate() {
-    // let uid=window.localStorage.getItem("uid");
-    // if (uid==null){
-    //  sendRedirect();
-    // }
+
   },
   created() {
-    // let uid=localStorage.getItem("uid")
-    // let token =window.sessionStorage.getItem("token")
-    // if (token==null&&uid!=null){
-    //   this.getToken;
-    // }
 
   },
   data(){
     return{
-      uid:window.localStorage.getItem("uid"),
-      allrecodes:[]
+
     }
   },
   methods:{
-    // getToken(){
-    //   let that=this;
-    //   axios.post('http://120.25.121.177:443/api/oauth/get_accesstoke/',{
-    //     "uid":that.uid
-    //   }).then(res=>{
-    //     if (res.data.message==="success"){
-    //       let token=res.data.access_token;
-    //       window.sessionStorage.setItem("token",token);
-    //       that.getAllinfo();
-    //     }
-    //   })
-    // },
+
 
   }
 }
@@ -63,4 +42,5 @@ export default {
   color: #2c3e50;
   margin-top: 0px;
 }
+
 </style>
